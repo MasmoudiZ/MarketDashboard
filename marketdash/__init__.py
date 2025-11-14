8c37c8f100440b4683290dee0eedb8525e6d1ee0
zmasmoudi <zmasmoudi@outlook.fr>
2025-11-14 09:47:23 +0100

    Expose load_config in marketdash package
    Added load_config to the public API and updated docstring.

--- marketdash/__init__.py
+++ marketdash/__init__.py
@@ -1,4 +1,10 @@
 # Makes "marketdash" a package and exposes public API
 from .providers import yf_history, te_history, te_get
 from .utils import compute_perf_table, load_yaml, ensure_dir, last_business_day
-from .visuals import plot_sector_panels
+"""
+Package marketdash : config & outils internes.
+"""
+    
+from .config import load_config
+
+__all__ = ["load_config"]
